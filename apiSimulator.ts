1ee3e108bd74d089481ae883b651ed8e6a110ba0
Abenezer <abitysank@gmail.com>
2025-11-19 14:05:56 -0500

    custome error class

--- apiSimulator.ts
+++ apiSimulator.ts
@@ -53,3 +53,17 @@ export const fetchSalesReport = (): Promise<{
         }, 1000);
     });
 };
+
+export class NetworkError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "NetworkError";
+    }
+}
+
+export class DataError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "DataError";
+    }
+}
